639d41101927aef4be148ef221e05a43bb1622df
muhtor <weboasis9002@gmail.com>
2020-10-27 15:28:59 +0500

    added function write_json() for write .json file

--- tutorial/apps/file_methods/models.py
+++ tutorial/apps/file_methods/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from .utils import open_file_with_mode, deleting_file
+from .utils import open_file_with_mode, deleting_file, write_json
 
 # Create your models here.
 file_r = 'apps/file_methods/demo.txt'
 file_a = 'apps/file_methods/demo_2.txt'
-print("Open and read....", open_file_with_mode(file=file_r, mode='r'))
+# print("Open and read....", open_file_with_mode(file=file_r, mode='r'))
 
-deleting_file(file=file_a)
+# deleting_file(file=file_a)
+
+
+payload = {
+  "access_token": "Some-TOKEN-@@@333444",
+  "expires_in": 7200,
+  "token_type": "Application Access Token"
+}
+file_json = 'apps/file_methods/auth_data'
+
+write_json(file=file_json, payload=payload)
--- tutorial/apps/file_methods/utils.py
+++ tutorial/apps/file_methods/utils.py
@@ -1,25 +1,26 @@
 import os
+import json
 
 
 def open_file_with_mode(file, mode: str = 'r'):
-    if mode == 'r':     # "r" - Read - Default value. Opens a file for reading, error if the file does not exist
+    if mode == 'r':  # "r" - Read - Default value. Opens a file for reading, error if the file does not exist
         f = open(file, mode)
         return f.read()
         # return f.read(5)  Return the 5 first characters of the file:
 
-    elif mode == 'a':   # "a" - Append - Opens a file for appending, creates the file if it does not exist
+    elif mode == 'a':  # "a" - Append - Opens a file for appending, creates the file if it does not exist
         f = open(file, mode)
         f.write("Now the file has more content!")
         return f.read()
-    elif mode == 'w':   # "w" - Write - Opens a file for writing, creates the file if it does not exist
+    elif mode == 'w':  # "w" - Write - Opens a file for writing, creates the file if it does not exist
         f = open(file, mode)
         f.write("Woops! I have deleted the content!")
         return f
-    elif mode == 'x':   # "x" - Create - Creates the specified file, returns an error if the file exist
+    elif mode == 'x':  # "x" - Create - Creates the specified file, returns an error if the file exist
         pass
-    elif mode == 't':   # "t" - Text - Default value. Text mode
+    elif mode == 't':  # "t" - Text - Default value. Text mode
         pass
-    elif mode == 'b':   # "b" - Binary - Binary mode (e.g. images)
+    elif mode == 'b':  # "b" - Binary - Binary mode (e.g. images)
         pass
     else:
         raise ValueError("Unexpected mode")
@@ -39,3 +40,10 @@ def deleting_folder(folder: str = ''):
         print("Folder deletion completed successfully")
     else:
         print("The Folder does not exist")
+
+
+def write_json(file: str = '', payload: dict = ()):
+    # with open(file + '.json', '+a') as outfile:
+    #  +a -> - Append - Opens a file for appending, creates the file if it does not exist
+    with open(file + '.json', 'w') as outfile:
+        json.dump(payload, outfile)
