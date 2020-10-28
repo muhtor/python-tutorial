aa36f4d56a61dbf96b5ceaf67f97f188ef8e6b65
muhtor <weboasis9002@gmail.com>
2020-10-28 17:59:59 +0500

    added new function for write JSON file

--- tutorial/apps/file_methods/models.py
+++ tutorial/apps/file_methods/models.py
@@ -1,19 +1,9 @@
 from django.db import models
-from .utils import open_file_with_mode, deleting_file, write_json
+from .utils import open_file_with_mode, deleting_file, write_json, calculate_time
 
 # Create your models here.
 file_r = 'apps/file_methods/demo.txt'
 file_a = 'apps/file_methods/demo_2.txt'
 # print("Open and read....", open_file_with_mode(file=file_r, mode='r'))
-
 # deleting_file(file=file_a)
-
-
-payload = {
-  "access_token": "Some-TOKEN-@@@333444",
-  "expires_in": 7200,
-  "token_type": "Application Access Token"
-}
-file_json = 'apps/file_methods/auth_data'
-
-write_json(file=file_json, payload=payload)
+calculate_time()
--- tutorial/apps/file_methods/utils.py
+++ tutorial/apps/file_methods/utils.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime, timedelta
 
 
 def open_file_with_mode(file, mode: str = 'r'):
@@ -42,8 +43,48 @@ def deleting_folder(folder: str = ''):
         print("The Folder does not exist")
 
 
-def write_json(file: str = '', payload: dict = ()):
+# WRITE AND UPDATE JSON FILE
+def write_json():
     # with open(file + '.json', '+a') as outfile:
     #  +a -> - Append - Opens a file for appending, creates the file if it does not exist
-    with open(file + '.json', 'w') as outfile:
+    count_json = 'apps/file_methods/count.json'
+    with open(count_json, 'r') as f:
+        data = json.loads(f.read())
+        counter = data['count_token']
+    file_json = 'apps/file_methods/count'
+    with open(file_json + '.json', 'w') as o:
+        count = {"count_token": counter + 1}
+        json.dump(count, o)
+
+    file_json = 'apps/file_methods/auth_data'
+    start_time = datetime.now().time().strftime('%H:%M:%S')
+    end_date = datetime.now() + timedelta(seconds=55) - timedelta(seconds=5)
+    expires_in = timedelta(seconds=55) - timedelta(seconds=5)
+    payload = {
+        "access_token": "Some-TOKEN-@@@333444",
+        "start_time": str(start_time),
+        "end_time": str(end_date.strftime('%H:%M:%S')),
+        "expires_in": str(expires_in),
+        "token_type": "Application Access Token"
+    }
+    with open(file_json + '.json', 'w') as outfile:
         json.dump(payload, outfile)
+
+
+def calculate_time():
+    file_json = 'apps/file_methods/auth_data.json'
+    with open(file_json, 'r') as f:
+        data = json.loads(f.read())
+        end_time = datetime.strptime(data['end_time'], '%H:%M:%S')
+        # difference = (t_4 - t_3)
+        # difference_time = difference.seconds + difference.seconds // 3600
+        current_time = datetime.strptime(datetime.now().strftime('%H:%M:%S'), '%H:%M:%S')
+        print("current_time...", current_time.time(), type(current_time.time()))
+        print("end_time...", end_time.time(), type(end_time.time()))
+        if current_time.time() > end_time.time():
+            print("Need to be updated .......")
+            write_json()
+            return True
+        else:
+            print("No need to update......")
+            return False
